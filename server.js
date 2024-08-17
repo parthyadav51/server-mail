af25f7c8ebedc2c4daeb7f8e93588ee2b627c1af
parthyadav51 <104004619+parthyadav51@users.noreply.github.com>
2024-08-17 19:02:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -14,22 +14,8 @@ app.use(bodyParser.json());
 
 // CORS configuration
 const corsOptions = {
-  origin: (origin, callback) => {
-    if (process.env.NODE_ENV === 'development') {
-      callback(null, true); // Allow all origins during development
-    } else {
-      const allowedOrigins = ['http://your-production-domain.com', 'http://another-allowed-domain.com'];
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-};
-
+  origin: "*"
+}
 app.use(cors(corsOptions));
 
 // Import routes
@@ -40,5 +26,5 @@ app.use('/api', emailRoutes);
 
 // Start the server
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Server is running on http://10.5.163.171:${port}`);
+  console.log(`Server is running on http://0.0.0.0:${port}`);
 });
